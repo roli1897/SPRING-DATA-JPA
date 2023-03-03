063d37c81d329c7a14c2fba18ac3972bd3bd74ee
yuan6 <yuan@gmail.com>
2023-03-03 21:32:17 +0100

    SPRING DATA JPA

---
+++ src/main/java/cn/itcast/domain/Customer.java
@@ -0,0 +1,74 @@
+package cn.itcast.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cst_customer")
+public class Customer {
+    private long custId;
+    private String custName;
+    private String custSource;
+    private String custLevel;
+    private String custIndustry;
+    private String custPhone;
+    private String custAddress;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cust_id")
+    public long getCustId() {
+        return custId;
+    }
+
+    public void setCustId(long custId) {
+        this.custId = custId;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public String getCustSource() {
+        return custSource;
+    }
+
+    public void setCustSource(String custSource) {
+        this.custSource = custSource;
+    }
+
+    public String getCustLevel() {
+        return custLevel;
+    }
+
+    public void setCustLevel(String custLevel) {
+        this.custLevel = custLevel;
+    }
+
+    public String getCustIndustry() {
+        return custIndustry;
+    }
+
+    public void setCustIndustry(String custIndustry) {
+        this.custIndustry = custIndustry;
+    }
+
+    public String getCustPhone() {
+        return custPhone;
+    }
+
+    public void setCustPhone(String custPhone) {
+        this.custPhone = custPhone;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+}
